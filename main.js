2c350bba1f3688fa53407d4a468d4aeae79fd2e3
EconClass <bizmail327@gmail.com>
2019-07-09 16:39:05 -0700

    [add] random stuff

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ function drawRectangle () {
 let x = canvas.width/2;
 let y = canvas.height-30;
 let ballRadius = 10;
+let ballColor = "#0095DD";
 
 let dx = 2;
 let dy = -2;
@@ -23,7 +24,7 @@ let dy = -2;
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-  ctx.fillStyle = "#0095DD";
+  ctx.fillStyle = ballColor;
   ctx.fill();
   ctx.closePath();
 };
@@ -34,14 +35,31 @@ function draw() {
 
   if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
     dx = -dx;
+    ballColor = randomColor();
+    // ballRadius = randomInteger();
   }
-  
+
   if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
     dy = -dy;
+    ballColor = randomColor();
+    // ballRadius = randomInteger();
   }
 
   x += dx;
   y += dy;
 };
+
+function randomColor() {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function randomInteger() {
+  return Math.floor((Math.random() * 20) + 5);
+}
 
 setInterval(draw, 10);
