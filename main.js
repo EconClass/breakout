7b3530153bfa143d9d0d57f1fca6de63128ac478
EconClass <bizmail327@gmail.com>
2019-07-09 18:09:38 -0700

    [add] random stuff

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
+//======================= Blocks =======================
 function drawRectangle () {
   ctx.beginPath();
   ctx.rect(20, 40, 50, 50);
@@ -12,14 +13,9 @@ function drawRectangle () {
   ctx.closePath();
 }
 
-
-let x = canvas.width/2;
-let y = canvas.height-30;
+//======================= Ball =======================
 let ballRadius = 10;
 let ballColor = "#0095DD";
-
-let dx = 2;
-let dy = -2;
 
 function drawBall() {
   ctx.beginPath();
@@ -29,10 +25,79 @@ function drawBall() {
   ctx.closePath();
 };
 
+//======================= Randomizers =======================
+function randomColor() {
+
+  // Random HEX
+  // let letters = '0123456789ABCDEF';
+  // let color = '#';
+  // for (let i = 0; i < 6; i++) {
+  //   color += letters[Math.floor(Math.random() * 16)];
+  // }
+
+  // Random RGBA
+  // let red = Math.floor(Math.random() * 255);
+  // let blue = Math.floor(Math.random() * 255);
+  // let green = Math.floor(Math.random() * 255);
+
+  // let color = `rgba(${red}, ${blue}, ${green}, 0.67)`;
+
+  // Random HSLA
+  let hue = Math.floor(Math.random() * 360);
+  let color = `hsla(${hue}, 100%, 50%, 0.67)`;
+
+  return color;
+}
+
+function randomInteger() {
+  return Math.floor((Math.random() * 20) + 5);
+} 
+
+
+//======================= Paddle =======================
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width-paddleWidth) / 2;
+
+let rightPressed = false;
+let leftPressed = false;
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function keyDownHandler(e) {
+  if(e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = true;
+  }
+  else if(e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if(e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = false;
+  }
+  else if(e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = false;
+  }
+}
+
+//======================= Draw =======================
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
-
+  // Ball Bounce
   if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
     dx = -dx;
     ballColor = randomColor();
@@ -47,19 +112,18 @@ function draw() {
 
   x += dx;
   y += dy;
+  
+  // Paddle movement
+  drawPaddle();
+  
+  if(rightPressed) {
+    paddleX += 7;
+  }
+  else if(leftPressed) {
+    paddleX -= 7;
+  }
 };
 
-function randomColor() {
-  let letters = '0123456789ABCDEF';
-  let color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-function randomInteger() {
-  return Math.floor((Math.random() * 20) + 5);
-}
-
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 setInterval(draw, 10);
